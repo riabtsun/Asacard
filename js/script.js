ca281416ba8ec5aa26b00469019f194f376142c5
dryabtsun <riabtsun@i.ua>
2024-07-18 12:04:57 +0300

    added smart header

--- js/script.js
+++ js/script.js
@@ -13,6 +13,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
+let lastScroll = 0
+const defaultOffset = 200
+const header = document.querySelector('.header')
+const scrollPosition =()=>window.pageYOffset || document.documentElement.scrollTop
+const containHide=()=>header.classList.contains('header-hide')
+window.addEventListener('scroll',()=>{
+  if (scrollPosition()>lastScroll && !containHide()){
+    header.classList.add('header-hide')
+  }
+  else if(scrollPosition()<lastScroll && containHide()){
+    header.classList.remove('header-hide')
+  }
+  else{}
+  lastScroll=scrollPosition()
+})
+
+
+
 //scroll to top
 let scrollToTopVisible = false;
 let scrollingNavbar = document.querySelector('#scrollingNavbar')
