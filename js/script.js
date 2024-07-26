4bd1cc598ae3e31fef03312110a4574711c217bd
dryabtsun <riabtsun@i.ua>
2024-07-26 16:47:53 +0300

    fixed header on the top

--- js/script.js
+++ js/script.js
@@ -13,21 +13,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-let lastScroll = 0
-const defaultOffset = 200
-const header = document.querySelector('.header')
-const scrollPosition =()=>window.pageYOffset || document.documentElement.scrollTop
-const containHide=()=>header.classList.contains('header-hide')
-window.addEventListener('scroll',()=>{
-  if (scrollPosition()>lastScroll && !containHide()){
-    header.classList.add('header-hide')
-  }
-  else if(scrollPosition()<lastScroll && containHide()){
-    header.classList.remove('header-hide')
-  }
-  else{}
-  lastScroll=scrollPosition()
-})
+// let lastScroll = 0
+// const defaultOffset = 200
+// const header = document.querySelector('.header')
+// const scrollPosition =()=>window.pageYOffset || document.documentElement.scrollTop
+// const containHide=()=>header.classList.contains('header-hide')
+// window.addEventListener('scroll',()=>{
+//   if (scrollPosition()>lastScroll && !containHide()){
+//     header.classList.add('header-hide')
+//   }
+//   else if(scrollPosition()<lastScroll && containHide()){
+//     header.classList.remove('header-hide')
+//   }
+//   else{}
+//   lastScroll=scrollPosition()
+// })
 
 
 
